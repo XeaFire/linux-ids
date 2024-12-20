be702e7081176136f63b2daa111e7f38ea917437
DTristan <tristan.diard@ynov.com>
2024-12-20 10:51:43 +0100

    HashManager:GetHash patch

--- packages/utils/hash_manager.py
+++ packages/utils/hash_manager.py
@@ -3,13 +3,12 @@ from hashlib import sha512, sha256, md5
 class HashManager:
     def GetHash(filepath : str, hash = "sha256") -> str:
         f = open(filepath, "r")
-        print(f)
         r = ""
         if (hash == "sha512"):
-            r = sha512(filepath.encode('utf-8')).hexdigest()
+            r = sha512(f.read().encode('utf-8')).hexdigest()
         elif (hash == "md5"):
-            r = md5(filepath.encode('utf-8')).hexdigest()
+            r = md5(f.read().encode('utf-8')).hexdigest()
         else:
-            r = sha256(filepath.encode('utf-8')).hexdigest()
+            r = sha256(f.read().encode('utf-8')).hexdigest()
         return r
 
