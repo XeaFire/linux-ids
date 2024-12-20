71e9fc88f9f888e17ea31491c9ede04dce12de75
DTristan <tristan.diard@ynov.com>
2024-12-20 12:12:49 +0100

    HashManager:GetHash patch

--- ids.py
+++ ids.py
@@ -4,7 +4,7 @@ def main():
     ben = input("plop")
     print(utils.FileManager.GetHash(ben, "sha256"))
     data = utils.FileManager.GetAllInfos(ben)
-    utils.JsonManager.Write("./ben/test.json", data)
+    utils.JsonManager.Write("./test.json", data)
     
 
 if __name__ == "__main__":
