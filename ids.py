3a72db17baa8e2d4bc2343b2f944f74fd8be5683
DTristan <tristan.diard@ynov.com>
2025-01-13 10:22:20 +0100

    JsonManager:Parse

--- ids.py
+++ ids.py
@@ -4,8 +4,9 @@ def main():
     ben = input("plop")
     print(utils.FileManager.GetHash(ben, "sha256"))
     data = utils.FileManager.GetAllInfos(ben)
-    utils.JsonManager.Write("./test.json", data)
-    print(utils.JsonManager.Parse("./test.json"))
+    utils.JsonManager.Write("/var/ids/db.json", data)
+    print(utils.JsonManager.Parse("/var/ids/db.json"))
+    print(utils.JsonManager.VerifyJson("/var/ids/db.json"))
     
 
 if __name__ == "__main__":
--- packages/utils/json_manager.py
+++ packages/utils/json_manager.py
@@ -10,5 +10,18 @@ class JsonManager:
         with open(pathfile, 'w') as f:
             json.dump(data, f)
 
-    def UpdateDB():
+    def EditKey(pathfile : str, key : str, data):
         pass
+ 
+    def DeleteKey(pathfile : str, key : str, data):
+        pass
+
+    def UpdateDB(data):
+        pass
+
+    def VerifyJson(pathfolder : str, data):
+        old_data = JsonManager.Parse(pathfolder)
+        if (old_data == data):
+            return True
+        else:
+            return False
