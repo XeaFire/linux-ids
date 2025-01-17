c52c8dd96fc980d59ee3758326ea79bcce6b1c98
DTristan <tristan.diard@ynov.com>
2025-01-17 02:52:12 +0100

    IDS.check

--- ids.py
+++ ids.py
@@ -3,10 +3,15 @@ import sys
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "check":
-        print("ben")
-    elif len(sys.argv) > 1 and sys.argv[1] == "prepare":
         data = utils.JsonManager.PrepareData()
         utils.JsonManager.Write("/var/ids/db.json", data)
+        old_data = utils.JsonManager.Parse("/var/ids/db.json")
+        if old_data == data :
+            print("State : Ok")
+        else:
+            print("State : Divergent")
+    elif len(sys.argv) > 1 and sys.argv[1] == "build":
+        pass
 
 if __name__ == "__main__":
     main()
