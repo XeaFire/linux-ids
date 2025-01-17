67a51fa6c7bac5e2b30fd4954bd490e6281dd9a4
DTristan <tristan.diard@ynov.com>
2025-01-17 12:23:19 +0100

    Bonus Discord v1

--- packages/utils/bonus_manager.py
+++ packages/utils/bonus_manager.py
@@ -6,6 +6,7 @@ class BonusManager:
     def SendDiscordAlert(msg : str):
         config = JsonManager.Parse("ids-config.json")
         pattern = re.compile("/discordapp.com\/api\/webhooks\/([^\/]+)\/([^\/]+)/")
+        print(config["discord-webhook"])
         if not pattern.match(config["discord-webhook"]):
             print("ben")
             return
